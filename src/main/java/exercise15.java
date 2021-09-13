bb61c9bef55b22e4bfc5c0c40d758334d888c9a2
Rachel Cameron <rachleecameron@gmail.com>
2021-09-13 17:21:56 -0400

    Commit

--- src/main/java/exercise15.java
+++ src/main/java/exercise15.java
@@ -8,5 +8,11 @@ import java.util.Scanner;
 public class exercise15 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        System.out.println("What is the password?");
+        String password = input.nextLine();
+        if(password.equals("abc$123"))
+            System.out.printf("Welcome!");
+            else
+                System.out.printf("I don't know you.");
     }
 }
